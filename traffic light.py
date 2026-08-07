fb7160cf6ab13fe81da67413058b5677c75dd4b3
rohitphulari8 <rohitphulari8@gmail.com>
2026-08-07 16:11:43 +0530

    Add files via upload

---
+++ traffic light.py
@@ -0,0 +1,11 @@
+print("********** Traffic Signal **********")
+colour=str(input("Traffic Light colour = "))
+
+if(colour=="Green"):
+    print("Go")
+elif(colour=="Yellow"):
+    print("Wait")
+elif(colour=="Red"):
+    print("Stop")
+else:
+    print("Traffic Signal not Working.")
