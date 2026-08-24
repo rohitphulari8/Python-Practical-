464afac38fe6e4f378932d8f7699b7360922e805
rohitphulari8 <rohitphulari8@gmail.com>
2026-08-24 10:47:43 +0530

    Add files via upload

---
+++ Pra E1 Q2.py
@@ -0,0 +1,13 @@
+print("----- EMPLOYEE INFORMATION ------")
+
+name = input("enter employee name  :-")
+roll = int(input("enter employee roll  :-"))
+salary = float(input("enter monthly salary  :-"))
+
+print("----- EMPLOYEE IDENTITY CARD -----")
+
+print("Employee Name is :", name)
+print("Employee Roll is :", roll)
+print("Monthly Salary is :", salary)
+
+print("----- THANK YOU -----")
