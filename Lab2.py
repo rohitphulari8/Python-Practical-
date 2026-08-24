e175beed52d0afd334f867291e4ca28eaeb44397
rohitphulari8 <rohitphulari8@gmail.com>
2026-08-24 10:59:20 +0530

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,21 +1,43 @@
-print("**********Admmission Eligibility Check**********")
-age=int(input("Enter your Age="))
-mark=float(input("Enter your Marks="))
+rice_price = 60
+sugar_price = 45
+salt_price = 20
+oil_price = 150
 
-if(age>=18 and age<=25):
-    print("Age is Eligible.")
 
-    if(mark>=60):
-       print("It is Eligible for Engineering.")
-      
-       if(mark>=85):
-            print("It is Eligible for AIML.")
-       elif(mark<85 and mark>=75):
-            print("It is Eligible for CSE and ENT&C.")
-       else:
-           print("It is Eligible for MECH,CIVIL,ELECTRICAL.")
-    else:
-        print("It is not Eligible for Engineering Admmission.")
+rice_qty = float(input("Enter Rice Quantity (kg): "))
+sugar_qty = float(input("Enter Sugar Quantity (kg): "))
+salt_qty = float(input("Enter Salt Quantity (kg): "))
+oil_qty = float(input("Enter Oil Quantity (liters): "))
+
+
+rice_total = rice_qty * rice_price
+sugar_total = sugar_qty * sugar_price
+salt_total = salt_qty * salt_price
+oil_total = oil_qty * oil_price
+
+print("\n************* Bill Detail *************")
+print("Rice:", rice_total)
+print("Sugar:", sugar_total)
+print("Salt:", salt_total)
+print("Oil:", oil_total)
+
+
+Total_Bill = rice_total + sugar_total + salt_total + oil_total
+print("Total Bill:", Total_Bill)
+
+print("=========== Final Discount ============")
+Discount = 0
+
+if Total_Bill >= 2000:
+    Discount = Total_Bill * 0.10
+elif Total_Bill >= 1000:
+    Discount = Total_Bill * 0.05
 else:
-    print("Age is not Eligible.")
-print("********** Thank You **********")
+    print("No Discount")
+
+if Discount > 0:
+    print("Discount:", Discount)
+
+print("=========== Final Bill ============")
+Final_Bill = Total_Bill - Discount
+print("Final Bill:", Final_Bill)
